b9d553097c2c5014b642d40f350b5bfdbc0fb45b
User <user@yandex.ru>
2021-01-30 13:21:43 +0700

    Another minor refactoring

--- Lab2/src/sublab1/core.clj
+++ Lab2/src/sublab1/core.clj
@@ -4,40 +4,51 @@
   [a b h]
   (* h (* 0.5 (+ a b))))
 
-(def integrate-memoized
-      (memoize
-        (fn [f n step rvalue]
-          (if (= n 0)
-            0
-            (let [lvalue (f (* (dec n) step))]
-              (+
-                (area lvalue rvalue step)
-                (integrate-memoized f (dec n) step lvalue)))))))
-
 (defn integrate
   "Функция численно интегрирует функцию f от 0.0 до x с шагом step"
   [f step]
   {:pre [(>= step 0.0)]}
-  (fn [x]
-    {:pre [(>= x 0.0)]}
-    (let [n (int (quot x step))
-          rvalue (f (* (dec n) step))]
-      (integrate-memoized f n step rvalue))))
+  (let [integrate-memoized (memoize
+                             (fn fn [f n step rvalue]
+                               (if (= n 0)
+                                 0
+                                 (let [lvalue (f (* (dec n) step))]
+                                   (+
+                                     (area lvalue rvalue step)
+                                     (integrate-memoized f (dec n) step lvalue))))))]
+    (fn [x]
+      {:pre [(>= x 0.0)]}
+      (let [n (int (quot x step))
+            rvalue (f (* (dec n) step))]
+        (integrate-memoized f n step rvalue)))))
 
 (defn slow-function
-  [x]
-  (Thread/sleep 1)
-  1)
+  [v]
+  (fn [x]
+    (Thread/sleep 1)
+    v))
 
 (defn main [& args]
-  (let [integrate-function (integrate slow-function 0.01)]
+  (let [integrate-function (integrate (slow-function 4) 0.01)
+        integrate-function2 (integrate (slow-function 8) 0.01)]
     (time (integrate-function 1))
     (time (integrate-function 0.25))
     (time (integrate-function 0.5))
     (time (integrate-function 1))
-    
+
     (println (integrate-function 0.00))
     (println (integrate-function 0.25))
     (println (integrate-function 0.50))
     (println (integrate-function 0.75))
-    (println (integrate-function 1.00))))
+    (println (integrate-function 1.00))
+
+    (time (integrate-function2 1))
+    (time (integrate-function2 0.25))
+    (time (integrate-function2 0.5))
+    (time (integrate-function2 1))
+
+    (println (integrate-function2 0.00))
+    (println (integrate-function2 0.25))
+    (println (integrate-function2 0.50))
+    (println (integrate-function2 0.75))
+    (println (integrate-function2 1.00))))
--- Lab2/src/sublab2/core.clj
+++ Lab2/src/sublab2/core.clj
@@ -17,19 +17,21 @@
   "Функция численно интегрирует функцию f от 0.0 до x с шагом step"
   [f step]
   {:pre [(>= step 0.0)]}
-  (def integrated-value-seq (integrated-value f step))
-  (fn [x]
-    {:pre [(>= x 0.0)]}
-    (let [n (int (quot x step))]
-      (nth integrated-value-seq n))))
+  (let [integrated-value-seq (integrated-value f step)]
+    (fn [x]
+      {:pre [(>= x 0.0)]}
+      (let [n (int (quot x step))]
+        (nth integrated-value-seq n)))))
 
 (defn slow-function
-  [x]
-  (Thread/sleep 1)
-  1)
+  [v]
+  (fn [x]
+    (Thread/sleep 1)
+    v))
 
 (defn main [& args]
-  (let [integrate-function (integrate slow-function 0.01)]
+  (let [integrate-function (integrate (slow-function 4) 0.01)
+        integrate-function2 (integrate (slow-function 8) 0.01)]
     (time (integrate-function 1))
     (time (integrate-function 0.25))
     (time (integrate-function 0.5))
@@ -39,6 +41,17 @@
     (println (integrate-function 0.25))
     (println (integrate-function 0.50))
     (println (integrate-function 0.75))
-    (println (integrate-function 1.00))))
+    (println (integrate-function 1.00))
+
+    (time (integrate-function2 1))
+    (time (integrate-function2 0.25))
+    (time (integrate-function2 0.5))
+    (time (integrate-function2 1))
+
+    (println (integrate-function2 0.00))
+    (println (integrate-function2 0.25))
+    (println (integrate-function2 0.50))
+    (println (integrate-function2 0.75))
+    (println (integrate-function2 1.00))))
 
 
